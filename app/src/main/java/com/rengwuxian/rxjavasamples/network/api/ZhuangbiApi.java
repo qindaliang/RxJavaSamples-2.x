2701e3467c4d67e5ef127a5cdc97a9f937944956
solexrd106 <1325716331@qq.com>
2018-09-04 16:49:33 +0800

    Initial commit

---
+++ app/src/main/java/com/rengwuxian/rxjavasamples/network/api/ZhuangbiApi.java
@@ -0,0 +1,19 @@
+// (c)2016 Flipboard Inc, All Rights Reserved.
+
+package com.rengwuxian.rxjavasamples.network.api;
+
+import com.rengwuxian.rxjavasamples.model.ZhuangbiImage;
+
+import java.util.List;
+
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import io.reactivex.Observable;
+
+public interface ZhuangbiApi {
+    @GET("search")
+    Observable<List<ZhuangbiImage>> search(@Query("q") String query);
+
+    @GET("search")
+    Observable<String> search1(@Query("q") String query);
+}
